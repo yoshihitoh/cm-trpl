275a81504087469c056c4488c566fd9f55411b55
Yoshihito Hirakawa <hirakawa.yoshihito@classmethod.jp>
2020-01-27 09:58:05 +0900

    第20回分の進捗を追加

--- chapter-12/minigrep/src/main.rs
+++ chapter-12/minigrep/src/main.rs
@@ -1,15 +1,43 @@
 use std::env;
+use std::error::Error as StdError;
 use std::fs;
+use std::io::Read;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|e| {
+        println!("Problem parsing arguments: {}", e);
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let filename = &args[2];
+    run(config);
+}
 
-    println!("Searching for {:?}", query);
-    println!("In file {:?}", filename);
-    let contents = fs::read_to_string(filename).expect("something went wrong reading the file");
+fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
+    let mut r = fs::File::open(&config.filename)?;
 
+    let mut s = String::new();
+    let contents = f.read_to_string(&mut s)?;
     println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
 }
